5b717abad9467b076f351aa8f557e9b5aa04af46
sabanferik <sbnftmaf@gmail.com>
2024-03-04 11:39:35 +0100

    ToDo importlar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <div className="container">
      
-      App
+     <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GörevEkle from "../components/GörevEkle"
+import GörevleriGöster from '../components/GörevleriGöster'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
